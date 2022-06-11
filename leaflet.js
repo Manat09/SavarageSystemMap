425e13ab291aea1ee4b1f8575ccefed470aa3b9f
Computer <youmydog75@gmail.com>
2022-06-11 12:10:47 +0600

    added example

--- leaflet.js
+++ leaflet.js
@@ -1,5 +1,39 @@
-var map = L.map('map').setView([51.090909, 71.418214], 14.5);
+var map = L.map('map').setView([51.090909, 71.418214], 14);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap by Manat and Andrey | IA-2002'
 }).addTo(map);
+var marker = L.marker([51.090909, 71.418214]).addTo(map);
+
+var circle = L.circle([51.098919, 71.398214], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+
+var polygon = L.polygon([
+    [51.099919, 71.428214],
+    [51.093919, 71.448214],
+    [51.100919, 71.462214]
+]).addTo(map);
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+var popup = L.popup()
+    .setLatLng([51.103919, 71.418214])
+    .setContent("I am a standalone popup.")
+    .openOn(map);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
